c673fbf89e69bbfef2b4d2631f871d192d75179e
Alptuğ <alptugidin@gmail.com>
2022-07-26 05:05:19 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from '@/layouts/layout';
+import Characters from '@/routes/characters';
+import Quotes from '@/routes/quotes';
+import Details from '@/routes/details';
 
-const App = () => {
+function App() {
   return (
-      <div>
-        <p>sss</p>
-      </div>
-  )
+    <div className="container mx-auto">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Characters />} />
+            <Route path="/details" element={<Details />} />
+            <Route path="/quotes" element={<Quotes />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
-
-export default App
+export default App;
