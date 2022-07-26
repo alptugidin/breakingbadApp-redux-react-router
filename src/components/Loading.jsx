c0cf10d8293d241da3a177f0c6797708084eb4e9
Alptuğ <alptugidin@gmail.com>
2022-07-26 16:59:14 +0300

    first

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Loading() {
+  return (
+    <div className="p-52">
+      <p className="text-5xl text-gray-600 font-semibold animate-pulse">Loading...</p>
+    </div>
+  );
+}
+
+export default Loading;
